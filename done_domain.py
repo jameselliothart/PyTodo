45095bb8d2d6e29eedc70135dad52a29ddbb778b
James Hart <james.elliot.hart@gmail.com>
2020-10-10 10:55:55 -0500

    isolate domain logic for completed items

---
+++ done_domain.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from typing import NamedTuple
+
+
+class CompletedItem(NamedTuple):
+    item: str
+    completed_on: datetime
+
+    def __str__(self) -> str:
+        return f'[{self.completed_on.isoformat()}] {self.item}'
+
+
+def create_default(item: str) -> CompletedItem:
+    return CompletedItem(item, datetime.now())
+
+def _start_of_day(date: datetime) -> datetime:
+    return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+def _start_of_week(date: datetime):
+    return _start_of_day(date - timedelta(days=date.isoweekday()))
+
+def days_ago(date: datetime, num_days: int) -> datetime:
+    return _start_of_day(date - timedelta(days=num_days))
+
+def weeks_ago(date: datetime, num_weeks: int) -> datetime:
+    return _start_of_week(date - timedelta(days=7*num_weeks))
